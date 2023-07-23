4e7d5b287beb0dd1d5ccbcbbd853a6fdc45475fb
ShreyashSiddharth <shreyashsiddharth@gmail.com>
2023-07-23 17:47:16 +0530

    Hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { restaurantList } from "../constants";
 import Resturantcard from "./ResturantCard";
 
+function filterData(searchTxt , restaurants){
+return restaurants.filter((restaurant) => 
+restaurant.data.name.includes(searchTxt)
+);
+}
+
+
 const Body = ()=>{
+    const [searchTxt, setSearchTxt] = useState("");
+    const [restaurants, setRestaurants] = useState(restaurantList);
+
     return(
+       
         <>
         <div className="search-container">
-            <input className="search-input" type="text" placeholder="Search" value="" />
-            <button className="search-btn" >Search</button>
+            <input 
+            className="search-input" 
+            type="text" 
+            placeholder="Search" 
+            value={searchTxt}
+            onChange={(e) => setSearchTxt(e.target.value)} 
+            />
+            <button className="search-btn" onClick={()=>{
+                //Data should be filtered
+                const data = filterData(searchTxt, restaurants);
+                //State to be updated
+                setRestaurants(data);
+
+            }} >Search</button>
         </div>
+      
         <div className='resturant-list'>
         {
-            restaurantList.map((restaurant)=>{
+            restaurants.map((restaurant)=>{
                 return  <Resturantcard {...restaurant.data} key ={restaurant.data.id}/>
             })
         }
